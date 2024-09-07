604e161ff77a2e31fc71abdca16884330dff42ad
eduxx123 <eduardolucas2310@gmail.com>
2024-09-07 15:58:52 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,39 @@
+let dados = [
+    {
+        titulo : "Meredith Grey",
+        descricao : "Meredith Grey é a personagem central da série de televisão médica Grey'sAnatomy. Interpretada porEllen Pompeo, Meredith é uma residente cirúrgica brilhante e determinada que enfrenta os desafios davida pessoal e profissional dentro do hospital Seattle Grace.",
+        link : "https://pt.wikipedia.org/wiki/Meredith_Grey",
+        especialidade : "Cirurgia Geral",
+        tags: "medusa merediti meriditi cirurgia geral"
+    
+    },
+    {
+        titulo: "Cristina Yang",
+        descricao: "Cristina Yang é uma das personagens mais icônicas de Grey's Anatomy. Conhecida por sua ambição e dedicação à cirurgia. Sua jornada na série é marcada por grandes desafios e conquistas.",
+        link: "https://pt.wikipedia.org/wiki/Cristina_Yang",
+        especialidade: "Cirurgia Cardiotorácica",
+        tags: "cardio iang chinesa china cirurgia cardiotorácica cardiotoracica"
+    },
+    {
+        titulo: "George O'Malley",
+        descricao: "George O'Malley é um dos internos da primeira temporada de Grey's Anatomy. Inicialmente inseguro e com dificuldades para se adaptar à vida no hospital, George cresce e se torna um médico competente e dedicado. Sua história na série é marcada por momentos emocionantes e inesperados.",
+        link: "https://pt.wikipedia.org/wiki/George_O'Malley",
+        tags: "007 jorge omaley acidente"
+    },
+    {
+        titulo: "Izzie Stevens",
+        descricao: "Izzie Stevens é uma residente cirúrgica de Grey's Anatomy, conhecida por sua natureza gentil e sonhadora. Sua jornada na série é marcada por desafios pessoais e profissionais.",
+        link: "https://pt.wikipedia.org/wiki/Izzie_Stevens",
+        especialidade: "Oncologia",
+        tags: "sonhadora gentil cancer oncologia"
+      },
+      {
+        titulo: "Alex Karev",
+        descricao: "Alex Karev é um dos internos da primeira temporada de Grey's Anatomy. Inicialmente rude e egoísta, Alex evolui ao longo da série, tornando-se um médico mais compassivo.",
+        link: "https://pt.wikipedia.org/wiki/Alex_Karev",
+        especialidade: "Cirurgia Pediátrica",
+        tags: "rude egoísta evolução pediatra"
+      },
+];
+
+
